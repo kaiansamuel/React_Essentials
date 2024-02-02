cd2003437e98c49acd89c2f44e1382bdc6471637
Kaian Samuel <kaiansamuelster@gmail.com>
2024-02-02 16:11:03 -0300

    Create buttons for childrens

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data.js'
 import Header from './components/Header/Header.jsx'; 
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
@@ -16,7 +17,15 @@ function App() {
             <CoreConcept{...CORE_CONCEPTS[2]} />
           </ul>
         </section>
-        
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>Jsx</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
