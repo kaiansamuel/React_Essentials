9842f473abd465aaaa3ab2cb3599ace895b5c095
Kaian Samuel <kaiansamuelster@gmail.com>
2024-02-04 17:42:44 -0300

    Create a Examples session

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-const TabButton = ({ children }) => {
-  return (
+const TabButton = ({ children, onSelect }) => {
+    return (
     <li>
-      <button>{ children }</button>
+      <button onClick={onSelect}>{ children }</button>
     </li>
   )
 }
